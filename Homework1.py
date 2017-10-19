196e36c09cabf638d7a122cbeef870ded5e404f5
Alex Pawlak <apawlak@uw.edu>
2017-10-19 16:31:25 -0700

    Re-organized and removed test print statements

--- Homework1.py
+++ Homework1.py
@@ -15,35 +15,29 @@ from collections import Counter
 import re
 import math
 
-# Read list of Vlog text files
-filelist = glob.glob('./transcripts/*.txt')
-
 # Initialize counters and dictionaries
 wordcount = Counter() #counter for words accross all docs
 docCount = Counter() #counter for number of docs each word occurs in
 top30TF = {} #dic for top 30 words TF
 top30IDF = {} #dic for top 30 words IDF
 top30TFIDF = {} #dic for top 30 words TF*IDF
-top30Prob = {} #dic for top 30 words probabilities
-
+top30Prob = {} #dic for top 30 words probabilities 
     
 # Read Volg text files, change all words to lowercase, and remove punctuation
-# Count occurances of words and how many docs they're present in
-for f in filelist:
-    file = open(f, "r")
-    text = file.read().lower()
-    file.close()
-    text = re.sub('[^a-z\ \']+', " ", text)
-    words = text.split()
-    wordcount.update(text.split())
-    docWords = []
-    for word in words:
-        if word not in docWords:
-            docWords.append(word)
-    docCount.update(docWords)
-
-# Open output file
-out = open('output.txt', "w")
+# Count occurances of each word and how many docs they're present in
+def count(filelist):   
+    for f in filelist:
+        file = open(f, "r")
+        text = file.read().lower()
+        file.close()
+        text = re.sub('[^a-z\ \']+', " ", text)
+        words = text.split()
+        wordcount.update(words)
+        docWords = []
+        for word in words:
+            if word not in docWords:
+                docWords.append(word)
+        docCount.update(docWords)
 
 # Returns number of Vlog documents
 def numDocs(filelist):
@@ -52,39 +46,61 @@ def numDocs(filelist):
 # Print word count for each word
 #for item in wordcount.items(): print("{}\t{}".format(*item))
 
-# Total number of words across all documents
-totalWordCount = sum(wordcount.values())
-out.write('Total Word Count: {}\n'.format(totalWordCount))
+# Returns total number of words across all documents
+def totalWordCount():
+    return sum(wordcount.values())    
 
-# Number of unique words across all documents
-uniqueWords = len(wordcount)
-out.write('Number of Unique Words: {}\n'.format(uniqueWords))
+# Returns number of unique words across all documents
+def uniqueWords():
+    return(len(wordcount))
 
 # Count words that only occur once
-occurOnce = 0
-for key in wordcount:
-    if wordcount[key] == 1:
-        occurOnce+=1
-out.write('Number of Words That Occur only Once: {}\n'.format(occurOnce))
+def occurOnce():
+    once = 0
+    for key in wordcount:
+        if wordcount[key] == 1:
+            once+=1
+    return(once)
 
 # Average number of words per document, rounded to nearest integer
-avgWords = round(sum(wordcount.values())/numDocs(filelist))
-out.write('Average Word Count Per Document: {}\n'.format(avgWords))
+def avgWords():
+    return(round(sum(wordcount.values())/numDocs(filelist)))
 
-# List of 30 most frequently used words and their counts (TF)
-top30 = wordcount.most_common(30)
+# Calculate TF, IDF, TF*IDF, probability for 30 most frequent words
+def mostFreq():
+    top30 = wordcount.most_common(30)
 
-# Calculate IDF, TF*IDF, probability for top 30 words
-for i in range(len(top30)):
-    tempWord = top30[i][0]
-    tempCount = top30[i][1]
-    tempDocCount = docCount[tempWord]
-    top30TF[tempWord] = tempCount
-    top30IDF[tempWord] = math.log10(numDocs(filelist)/tempDocCount)
-    top30TFIDF[tempWord] = tempCount * top30IDF[tempWord]
-    top30Prob[tempWord] = tempCount / totalWordCount
+    for i in range(len(top30)):
+        tempWord = top30[i][0]
+        tempCount = top30[i][1]
+        tempDocCount = docCount[tempWord]
+        top30TF[tempWord] = tempCount
+        top30IDF[tempWord] = math.log10(numDocs(filelist)/tempDocCount)
+        top30TFIDF[tempWord] = tempCount * top30IDF[tempWord]
+        top30Prob[tempWord] = tempCount / totalWordCount()
 
-# Write TF, IDF, TF*IDF, Probabilities of top 30 words
+# Read list of Vlog text files
+filelist = glob.glob('./transcripts/*.txt')
+
+# Process word counts in all documents
+count(filelist)
+
+# Run and store counts and calculations
+total = totalWordCount()
+unique = uniqueWords()
+once = occurOnce()
+avg = avgWords()
+mostFreq()
+
+# Open output file
+out = open('output.txt', "w")
+
+# Write results to output file
+out.write('Total Word Count: {}\n'.format(total))
+out.write('Number of Unique Words: {}\n'.format(unique))
+out.write('Number of Words That Occur only Once: {}\n'.format(once))
+out.write('Average Word Count Per Document: {}\n'.format(avg))
+
 out.write('30 Most Frequent Words:\n')
 out.write('Word\tTF\tIDF\tTF*IDF\tProbability\n')
 for key in top30TF:
@@ -93,6 +109,8 @@ for key in top30TF:
 
 # Close output file
 out.close()
+
+
     
 
     
